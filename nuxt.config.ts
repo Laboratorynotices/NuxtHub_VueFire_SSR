c826752c17be2d93ddb152a97317a057ee3d07c7
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-02-01 21:14:42 +0100

    feat(nuxt): configure VueFire SSR with .env support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxthub/core"],
+  modules: ["@nuxthub/core", "nuxt-vuefire"],
   ssr: true,
+  vuefire: {
+    config: {
+      apiKey: process.env.GOOGLE_FIREBASE_CONFIG_API_KEY,
+      authDomain: process.env.GOOGLE_FIREBASE_CONFIG_AUTH_DOMAIN,
+      projectId: process.env.GOOGLE_FIREBASE_CONFIG_PROJECT_ID,
+      appId: process.env.GOOGLE_FIREBASE_CONFIG_APP_ID,
+      // there could be other properties depending on the project
+    },
+  },
 });
