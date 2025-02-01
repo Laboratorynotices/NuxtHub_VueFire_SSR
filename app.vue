8e2f6c8e34d28a09c0b4ecb1caeec2413a520147
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-02-01 18:11:42 +0100

    feat(ssr): add SSR request handling in app.vue and hello API
    - Implement SSR request in `app.vue` and display the response
    - Enable SSR request handling in `server/api/hello.ts`

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
+    <div>
+      <pre>{{ data }}</pre>
+    </div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+const { data } = await useFetch("/api/hello");
+</script>
